5ea59ba392c4df30b45cd2543eebfb60a0ccfdd1
sergio <dg.sergio@gmail.com>
2021-08-28 17:55:51 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## GitHub Profiles App
+
+- Design: https://uidesigndaily.com/posts/photoshop-profile-card-user-day-286
+- search for user
+- get and show data
---
+++ app.js
@@ -0,0 +1,58 @@
+const urlUser = 'https://api.github.com/users/';
+
+const form = document.getElementById('formulario');
+const input = document.getElementById('entrada');
+
+form.addEventListener('submit', async e =>{
+    e.preventDefault();
+    const resultado = await getAPI(urlUser+input.value)
+    imprimir(resultado);
+});
+
+
+async function getAPI(url) {
+    const pedido = await fetch(url);
+    const resultado = await pedido.json();
+    return resultado;
+}
+
+async function imprimir(perfil) {    
+    const {avatar_url, name, company, bio, followers, following, public_repos, login} = perfil;    
+    const repos = await getAPI(urlUser+login+'/repos');    
+    console.log(perfil);
+    console.log(repos);
+    if (document.querySelector('.card')) document.querySelector('.card').remove();
+    const main = document.querySelector('main');
+    const divCard = document.createElement('div');    
+    divCard.classList.add('card');
+
+    divCard.innerHTML = `
+    <div class="perfil">
+        <div class="foto">
+            <img src="${avatar_url}" />
+        </div>
+        <div class="perfil-info">
+            <div class="general">
+                <h3>${name}</h3>
+                <small>${company}</small>
+                <p>${bio}</p>
+            </div>
+            <div class="iconos">
+                <div><i class="fas fa-heart"></i>${followers}</div>
+                <div><i class="fas fa-user"></i>${following}</div>
+                <div><i class="fas fa-code"></i>${public_repos}</div>
+            </div>                
+        </div>    
+    </div>
+    `;
+    const divRepos = document.createElement('div');
+    divRepos.classList.add('repos');    
+    repos.slice(0,9).forEach( e => {        
+        divRepos.innerHTML += `
+            <a href="${e.html_url}" target="_blank">${e.name}</a>
+        `
+    })
+
+    divCard.appendChild(divRepos);
+    main.appendChild(divCard);
+}
